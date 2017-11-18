58b2011a3cc6c7aff22b96a4dff1cb02f7928c78
wideday <mail.for.every.day@gmail.com>
2017-11-18 09:43:44 +0300

    edit test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #1
 cd 01_hello-world
-'if [ ! -d "./build" ];
-    then mkdir build;
-fi'
+#'if [ ! -d "./build" ];
+#    then mkdir build;
+#fi'
+mkdir build
 cd build
 cmake ..
 make
@@ -10,9 +11,10 @@ make
 
  #2
 cd ../../02_sdl-dynamic
-'if [ ! -d "./build" ];
-    then mkdir build;
-fi'
+#'if [ ! -d "./build" ];
+#    then mkdir build;
+#fi'
+mkdir build
 cd build
 cmake ..
 make
@@ -20,9 +22,10 @@ make
 
 #3
 cd ../../02_sdl-static
-'if [ ! -d "./build" ];
-    then mkdir build;
-fi'
+#'if [ ! -d "./build" ];
+#    then mkdir build;
+#fi'
+mkdir build
 cd build
 cmake ..
 make
