36103a5d12cc5beb3e34365faacd6bae1dfcb679
wideday <mail.for.every.day@gmail.com>
2017-12-02 02:00:02 +0300

    edit paths in test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ cd ../bin/Debug
 ./01_hello-world
 
  #2
-cd ../../02_sdl-dynamic
+cd ../../../02_sdl-dynamic
 #'if [ ! -d "./build" ];
 #    then mkdir build;
 #fi'
@@ -23,7 +23,7 @@ cd ../bin/Debug
 ./02_sdl-dynamic
 
 #3
-cd ../../02_sdl-static
+cd ../../../02_sdl-static
 #'if [ ! -d "./build" ];
 #    then mkdir build;
 #fi'
