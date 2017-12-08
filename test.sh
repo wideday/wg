28b55d92089a7363dd6f54ab154bbc1567df968d
wideday <mail.for.every.day@gmail.com>
2017-12-09 00:21:17 +0300

    edit build scripts for 05_texture_animation

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-#1
-cd 01_hello-world
+#5_2
+cd 05_texture-animation
 #'if [ ! -d "./build" ];
 #    then mkdir build;
 #fi'
@@ -8,58 +8,4 @@ cd build
 cmake ..
 make
 cd ../bin/Debug
-./01_hello-world
-
- #2_1
-cd ../../../02_sdl-dynamic
-#'if [ ! -d "./build" ];
-#    then mkdir build;
-#fi'
-mkdir build
-cd build
-cmake ..
-make
-cd ../bin/Debug
-./02_sdl-dynamic
-
-#2_2
-cd ../../../02_sdl-static
-#'if [ ! -d "./build" ];
-#    then mkdir build;
-#fi'
-mkdir build
-cd build
-cmake ..
-make
-cd ../bin/Debug
-./02_sdl-static
-
-#3_1
-cd ../../../03_sdl-loop
-#'if [ ! -d "./build" ];
-#    then mkdir build;
-#fi'
-mkdir build
-cd build
-cmake ..
-make
-
-#3_2
-cd ../03_sdl-loop-engine
-#'if [ ! -d "./build" ];
-#    then mkdir build;
-#fi'
-mkdir build
-cd build
-cmake ..
-make
-
-#3_3
-cd ../03_sdl-loop-engine-dll
-#'if [ ! -d "./build" ];
-#    then mkdir build;
-#fi'
-mkdir build
-cd build
-cmake ..
-make
+./05_vertex-morphing_game
